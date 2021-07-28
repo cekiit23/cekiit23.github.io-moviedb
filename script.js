f2dac0e0b6493096a09407b3dd5722b41afd6d7b
cekiit23 <yogabenrc@gmail.com>
2021-07-28 23:19:43 +0700

    change api request method to fetch()

--- script.js
+++ script.js
@@ -1,47 +1,75 @@
-$('.btn-input').on('click', function () {
-    // console.log($('.input-keyword').val())
-    $.ajax(
-        {
-            url: 'https://www.omdbapi.com/?apikey=dca61bcc&s=' + $('.input-keyword').val(),
-            success: results => {
-                const movie = results.Search;
+const searchButton = document.querySelector('.btn-search')
+searchButton.addEventListener('click',function() {
+    const movieKeyword = document.querySelector('.input-keyword')
+    const keyword = movieKeyword.value
     
-                let cards = ''
-                movie.forEach(m => {
-                    cards += `<div class="card col-md-4 my-3">
-                    <img src="${m.Poster}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                      <h5 class="card-title">${m.Title}</h5>
-                      <p class="card-text">${m.Year}</p>
-                      <button type="button"  class="btn btn-primary btn-modal" data-bs-toggle="modal" data-imdbid=${m.imdbID} data-bs-target="#modalDetail">Detail</button>
-                    </div>
-                  </div>`;
+    fetch(`https://www.omdbapi.com/?s=${keyword}&apikey=346fd566`)
+        .then(response => response.json()).then(response => {
+            const movies = response.Search
+
+            let movie = ''
+
+            movies.forEach(m => {
+                movie += showCards(m)
+            })
+
+            const movieContainer = document.querySelector('.movie-container')
+            movieContainer.innerHTML = movie; 
+
+            const detailButton = document.querySelectorAll('.btn-modal')
+            detailButton.forEach(db => {
+                db.addEventListener('click', function() {
+                    const imdbid = this.dataset.imdbid;
+                    fetch(`https://www.omdbapi.com/?i=${imdbid}&apikey=346fd566`)
+                        .then(response => response.json())
+                        .then( m => {
+                            let showDetail = ''
+                            const detailBody = document.querySelector('.detail-body')
+                            showDetail += showDetailModal(m)
+
+                            detailBody.innerHTML = showDetail
+
+
+                        })
                 })
-    
-                $('.movie-container').html(cards)
-    
-                $('.btn-modal').on('click', function(){
-                    $.ajax({
-                        url: 'https://www.omdbapi.com/?apikey=dca61bcc&i=' + $(this).data('imdbid'),
-                        success: m => {
-                            const movieDetail = `<h5>Title : </h5> ${m.Title}
-                                                 <h5>Released : </h5>  ${m.Released}
-                                                 <h5>Genre : </h5> ${m.Genre}
-                                                 <h5>Rated : </h5> ${m.Rated}                 
-                            
-                            
-                            
-                            `
-                            
-                            $('.modal-body').html(movieDetail);
-    
-                        }
-                    })
-    
-                    
-                })
-            }
-    
-        }
-    )
+            })
+        })
 })
+
+
+
+
+
+
+function showCards(m)
+{
+    return `
+    <div class="col-md-4">
+    <div class="card">
+    <img src="${m.Poster}" class="card-img-top" alt="..." style="width:350px;height="200px"">
+    <div class="card-body">
+      <h5 class="card-title">${m.Title}</h5>
+      <p class="card-text">${m.Year}</p>
+      <button type="button"  class="btn btn-primary btn-modal" data-bs-toggle="modal" data-imdbid=${m.imdbID} data-bs-target="#modalDetail">Detail</button>
+    </div>
+  </div>
+    </div>`
+} 
+
+function showDetailModal(m)
+{
+    return `
+    <div class="col-md">
+        <img src="${m.Poster}"
+    </div>
+    <div class="col-md">
+        <h2>${m.Title}</h2>
+        <p><b>Release :</b> ${m.Released}</p>
+        <p><b>Runtime : </b>${m.Runtime}</p>
+        <p><b>Genre : </b>${m.Genre}</p>
+        <p><b>Director : </b>${m.Director}</p>
+        <p><b>Writers : </b>${m.Writer}</p>
+        <p><b>Actors : </b>${m.Actors}</p>
+    </div>
+    `
+}
